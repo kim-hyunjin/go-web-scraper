a6d488b16200577ef607d4ba1a2e82566880f681
Kim <hyunjin1612@gmail.com>
2021-03-28 19:40:02 +0900

    about method

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kim-hyunjin/go-scraper
+
+go 1.16
--- main.go
+++ main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/kim-hyunjin/go-scraper/banking"
+)
+
+func main() {
+	account := banking.NewAccount("hyunjin")
+	account.Deposit(5000)
+	fmt.Println(account.Owner(), account.Balance())
+	account.Withdraw(3000)
+	account.ChangeOwner("gildong")
+	fmt.Println(account.Owner(), account.Balance())
+	fmt.Println(account)
+	err := account.Withdraw(3000)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
