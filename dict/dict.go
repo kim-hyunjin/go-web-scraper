f2bca9470ebea6907a5827d7cc56873ba698fde5
Kim <hyunjin1612@gmail.com>
2021-03-30 21:05:40 +0900

    딕셔너리 만들기 예제

--- dict/dict.go
+++ dict/dict.go
@@ -1,6 +1,11 @@
 package dict
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/kim-hyunjin/go-scraper/banking"
+)
 
 type Dictionary map[string]string
 
@@ -46,4 +51,48 @@ func (d Dictionary) Update(word, definition string) error {
 // Delete a word in dictionary
 func (d Dictionary) Delete(word string) {
 	delete(d, word)
+}
+
+func exam() {
+	account := banking.NewAccount("hyunjin")
+	account.Deposit(5000)
+	fmt.Println(account.Owner(), account.Balance())
+	account.Withdraw(3000)
+	account.ChangeOwner("gildong")
+	fmt.Println(account.Owner(), account.Balance())
+	fmt.Println(account)
+	// err := account.Withdraw(3000)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	dictionary := Dictionary{"first": "First word"}
+	dictionary["hello"] = "hello"
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+
+	err = dictionary.Add("hello2", "greeting")
+	if err != nil {
+		fmt.Println(err)
+	}
+	definition, err = dictionary.Search("hello2")
+	fmt.Println(definition)
+
+	err = dictionary.Update("hello2", "Hola!")
+	if err != nil {
+		fmt.Println(err)
+	}
+	definition, _ = dictionary.Search("hello2")
+	fmt.Println(definition)
+
+	dictionary.Delete("hello2")
+
+	err = dictionary.Update("hello2", "howdy!")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
